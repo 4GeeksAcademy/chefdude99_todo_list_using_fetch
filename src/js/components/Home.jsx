a896923eecc754d9637414a168e40403634a753f
chefdude99 <chefdude.1999@gmail.com>
2025-03-07 23:31:13 +0000

    medio ahi

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [Tareas, setTareas] = useState([]);
+	const [NuevasTareas, setNuevasTareas] = useState("")
+	function handleKeyPress(event) {
+		if (event.key === "Enter" && NuevasTareas.trim()) {
+			setTareas([...Tareas, NuevasTareas]);
+			setNuevasTareas("");
+		}
+	}
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Principal">
+			<h1>Todo's</h1>
+			<div className="input-group mb-3">
+				<input type="text" className="form-control" placeholder="Tareas por hacer" aria-label="Username" aria-describedby="basic-addon1" />
+			</div>
 		</div>
 	);
 };
