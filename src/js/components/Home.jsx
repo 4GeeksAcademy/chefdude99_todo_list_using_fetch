df7cb45ec6d6b1c4bd62eac61878d5fc067b34fc
chefdude99 <chefdude.1999@gmail.com>
2025-03-11 00:38:59 +0000

    listo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const [Tareas, setTareas] = useState([]);
-	const [NuevasTareas, setNuevasTareas] = useState("")
-	function handleKeyPress(event) {
-		if (event.key === "Enter" && NuevasTareas.trim()) {
-			setTareas([...Tareas, NuevasTareas]);
-			setNuevasTareas("");
-		}
-	}
+	const [tareas, setTareas] = useState([]);
 	return (
-		<div className="Principal">
+		<div className="Principal container">
 			<h1>Todo's</h1>
-			<div className="input-group mb-3">
-				<input type="text" className="form-control" placeholder="Tareas por hacer" aria-label="Username" aria-describedby="basic-addon1" />
+			<input type="text" className="form-control" placeholder="tareas por hacer"
+				onKeyDown={(event) => {
+					if (event.key == "Enter") {
+						setTareas([...tareas, event.target.value])
+					}
+				}}
+			/>
+			<ul className="list-group">
+				{tareas.map((tarea, index) =>
+					<li className="tarea-item list-group-item" key={index}>
+						<span className="texto ">{tarea}</span>
+						<button type="button" className="eliminar btn" onClick={() => {
+							let newTareas = [...tareas];
+							newTareas.splice(index, 1);
+							setTareas(newTareas)
+						}}>X</button>
+					</li>
+				)}
+			</ul>
+			<div className="tareas-pendi">
+				<p>quedan estas tareas:  </p>
+				{tareas.length}
 			</div>
 		</div>
 	);
-};
-
+}
 export default Home;
