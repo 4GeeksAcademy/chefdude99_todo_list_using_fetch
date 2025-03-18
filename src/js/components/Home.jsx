8e83f88f62bf1cbaa499b5b7293a60d53c61404e
chefdude99 <chefdude.1999@gmail.com>
2025-03-18 04:33:34 +0000

    listo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,24 +7,67 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 
 	useEffect(() => {
-		console.log("Hello")
 		fetch(`${apiUrl}/users/${userName}`)
 			.then((response) => response.json())
-			.then((data) => {
-				console.log(data) 
-				setTareas(data.todos)})
+			.then((data) => setTareas(data.todos))
 			.catch((error) => console.log(error))
 	}, [])
+
+	const addTodo = async (event) => {
+		if (event.key === 'Enter' && inputValue.trim() !== '') {
+			setInputValue('');
+			const respuesta = await fetch(`${apiUrl}/todos/${userName}`, {
+				method: "POST",
+				body: JSON.stringify({
+					"label": inputValue,
+					"is_done": false,
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+
+			})
+			const nuevaTodo = await respuesta.json()
+			console.log(nuevaTodo)
+			setTareas(oldTareas=>[...oldTareas,nuevaTodo])
+		}
+	}
+
+	const borrarUna = async (index, id) => {
+		let newTareas = [...tareas];
+		newTareas.splice(index, 1);
+		setTareas(newTareas)
+		await fetch(`${apiUrl}/todos/${id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}
+		)
+
+
+	}
+
+	const borrarTodos = () => {
+		tareas.map((tarea) => {
+			fetch(`${apiUrl}/todos/${tarea.id}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+			)
+		})
+		setTareas([])
+	}
+
 	return (
 		<div className="Principal container">
 			<h1>Todo's</h1>
 			<input type="text" className="form-control" placeholder="tareas por hacer" value={inputValue}
 				onChange={(event) => setInputValue(event.target.value)}
 				onKeyDown={(event) => {
-					if (event.key === 'Enter' && inputValue.trim() !== '') {
-						setTareas([...tareas, inputValue]);
-						setInputValue('');
-					}
+					addTodo(event)
 				}}
 			/>
 			<ul className="list-group">
@@ -32,9 +75,7 @@ const Home = () => {
 					<li className="tarea-item list-group-item" key={index}>
 						<span className="texto ">{tarea.label}</span>
 						<button type="button" className="eliminar btn" onClick={() => {
-							let newTareas = [...tareas];
-							newTareas.splice(index, 1);
-							setTareas(newTareas)
+							borrarUna(index, tarea.id);
 						}}>X</button>
 					</li>
 				)}
@@ -43,6 +84,10 @@ const Home = () => {
 				<p>quedan estas tareas:  </p>
 				{tareas.length}
 			</div>
+			<button className="btn btn-danger"
+				onClick={() => {
+					borrarTodos()
+				}}>ELIMINAR TODO</button>
 		</div>
 	);
 }
